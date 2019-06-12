490ea44e9007e8a387902e68ffa5a75a24bb7386
selonj <selonj@163.com>
2019-06-12 22:21:57 +0800

    RED: remove duplications with array containing same elements

--- src/test/java/ArrayTest.java
+++ src/test/java/ArrayTest.java
@@ -13,4 +13,9 @@ public class ArrayTest {
     public void returnImmediatelyWithArrayContainingUniqueElements() {
         assertThat(Array.unique(Array.of(1, 2, 3)), equalTo(Array.of(1, 2, 3)));
     }
+
+    @Test
+    public void removeDuplicationsWithArrayContainingSameElements() {
+        assertThat(Array.unique(Array.of(1, 1)), equalTo(Array.of(1)));
+    }
 }
