f9296410f54fed7050e97ba13e50190360e79116
selonj <selonj@163.com>
2019-06-12 22:15:43 +0800

    RED: unique an array which containing unique elements

---
+++ src/main/java/Array.java
@@ -0,0 +1,13 @@
+/**
+ * @author xiaoliang
+ * @since 2019-06-12
+ */
+public class Array {
+    public static int[] of(int... array) {
+        return array;
+    }
+
+    public static int[] unique(int[] array) {
+        return new int[0];
+    }
+}
---
+++ src/test/java/ArrayTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author xiaoliang
+ * @since 2019-06-12
+ */
+public class ArrayTest {
+
+    @Test
+    public void returnImmediatelyWithArrayContainingUniqueElements() {
+        assertThat(Array.unique(Array.of(1, 2, 3)), equalTo(Array.of(1, 2, 3)));
+    }
+}
