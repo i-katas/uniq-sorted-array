62a854fb097108d20db1bc32bf05fcb59cb31c08
selonj <selonj@163.com>
2019-06-12 23:05:00 +0800

    REFACTOR: replace while loop with for loop

--- src/main/java/Array.java
+++ src/main/java/Array.java
@@ -11,14 +11,11 @@ public class Array {
 
     public static int[] unique(int[] array) {
         int end = array.length - 1;
-        int i = 0;
-        while (i < end) {
-            int last = i + 1;
-            while (last <= end && array[i] == array[last]) {
+        for (int i = 0; i < end; i++) {
+            for (int last = i + 1; last <= end && array[i] == array[last]; ) {
                 System.arraycopy(array, last + 1, array, i + 1, end - last);
                 end--;
             }
-            i++;
         }
         return Arrays.copyOf(array, end + 1);
     }
