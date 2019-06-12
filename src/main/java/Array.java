d673e6e068b4927a76eb20d8ce25d2b1354fe6a1
selonj <selonj@163.com>
2019-06-12 23:09:11 +0800

    REFACTOR: compute the number of duplications in order for making the `arraycopy` out of the inner loop

--- src/main/java/Array.java
+++ src/main/java/Array.java
@@ -12,10 +12,12 @@ public class Array {
     public static int[] unique(int[] array) {
         int end = array.length - 1;
         for (int i = 0; i < end; i++) {
-            for (int last = i + 1; last <= end && array[i] == array[last]; ) {
+            int dups = 0;
+            for (int last = i + 1; last <= end && array[i] == array[last]; last++) {
+                dups++;
                 System.arraycopy(array, last + 1, array, i + 1, end - last);
-                end--;
             }
+            end -= dups;
         }
         return Arrays.copyOf(array, end + 1);
     }
