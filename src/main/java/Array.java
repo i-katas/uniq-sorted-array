a43197a6ab0bde04fd54d5adff9ba7b5064e7a02
selonj <selonj@163.com>
2019-06-12 22:24:18 +0800

    GREEN: make the test pass by comparing first 2 elements

--- src/main/java/Array.java
+++ src/main/java/Array.java
@@ -11,6 +11,9 @@ public class Array {
 
     public static int[] unique(int[] array) {
         int size = array.length;
+        if (array[0] == array[1]) {
+            size--;
+        }
         return Arrays.copyOf(array, size);
     }
 }
