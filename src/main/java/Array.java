479897492da9f116a7d6fd1f2e3c77a5dfe6ada9
selonj <selonj@163.com>
2019-06-12 23:20:52 +0800

    REFACTOR: inline all initial & post computation inside for loop

--- src/main/java/Array.java
+++ src/main/java/Array.java
@@ -10,15 +10,13 @@ public class Array {
     }
 
     public static int[] unique(int[] array) {
-        int end = array.length - 1;
-        for (int i = 0; i < end; i++) {
-            int dups = 0;
-            for (int last = i + 1; last <= end && array[i] == array[last]; last++) {
+        int size = array.length;
+        for (int i = 0, dups = 0; i < size; i++, size -= dups, dups = 0) {
+            for (int last = i + 1; last < size && array[i] == array[last]; last++) {
                 dups++;
             }
-            System.arraycopy(array, i + dups + 1, array, i + 1, end + 1 - (i + dups + 1));
-            end -= dups;
+            System.arraycopy(array, i + dups + 1, array, i + 1, size - (i + dups + 1));
         }
-        return Arrays.copyOf(array, end + 1);
+        return Arrays.copyOf(array, size);
     }
 }
